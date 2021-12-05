98e3599dcf0c55aba1c0f27569f9e58ca9ea511b
Yinuo Hu <huyinuo@live.unc.edu>
2021-12-05 16:22:10 -0500

    save result as txt

--- result/concat_result.py
+++ result/concat_result.py
@@ -1,14 +1,19 @@
 import pandas as pd
 
 df = pd.read_json("result.json")
-caps = {}
+caps = []
 for column in df:
     imgname = column.split("/")[3]
     columnSeriesObj = df[column]
     columncap = ''
     for i in range(len(columnSeriesObj)):
         columncap= columncap + columnSeriesObj[i]['cap'] + "; "
-    caps[imgname] = columncap
+    # caps[imgname] = columncap
+    caps.append(imgname + '\n')
+    caps.append(columncap + '\n')
 
-caps = pd.DataFrame(caps, index=[0])
-caps.to_json("densecap_result.json")
+with open("densecap_result.txt", 'w') as f:
+    f.writelines(caps)
+#
+# caps = pd.DataFrame(caps, index=[0])
+# caps.to_json("densecap_result.json")
